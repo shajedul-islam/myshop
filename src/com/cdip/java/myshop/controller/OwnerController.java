452592afed5e11ceb2cd447391ad7ccda711ab3c
Md. Shajedul Islam <shajedul.islam@robi.com.bd>
2018-09-28 03:28:59 +0600

    JavaFX panel, controller and DAO for creating owner

--- src/com/cdip/java/myshop/controller/OwnerController.java
+++ src/com/cdip/java/myshop/controller/OwnerController.java
@@ -1,4 +1,21 @@
 package com.cdip.java.myshop.controller;
 
+import com.cdip.java.myshop.dao.OwnerDAO;
+import com.cdip.java.myshop.model.Owner;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class OwnerController {
+
+    public TextField nameTextField;
+    public TextField phoneNumberTextField;
+
+    public void create(ActionEvent actionEvent) {
+        Owner owner = new Owner();
+        owner.setName(nameTextField.getText());
+        owner.setPhoneNumber(Long.parseLong(phoneNumberTextField.getText()));
+
+        OwnerDAO ownerDAO = new OwnerDAO();
+        ownerDAO.save(owner);
+    }
 }
--- src/com/cdip/java/myshop/dao/OwnerDAO.java
+++ src/com/cdip/java/myshop/dao/OwnerDAO.java
@@ -2,10 +2,33 @@ package com.cdip.java.myshop.dao;
 
 import com.cdip.java.myshop.model.Owner;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class OwnerDAO {
 
     public Owner save(Owner owner) {
         // save owner in database
+        // save owner in database
+        // connect to database
+
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/myshop_db?"
+                            + "user=root&password=root");
+
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO owner(name, phone_number) " +
+                    "VALUES ( '" + owner.getName() + "', " + owner.getPhoneNumber() + ")");
+
+
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
         return owner;
     }
 }
