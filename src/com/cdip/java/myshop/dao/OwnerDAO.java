2e07c30a59af14dd458d7f7134df6eac6d1bb615
Md. Shajedul Islam <shajedul.islam@robi.com.bd>
2018-09-28 04:08:22 +0600

    Using preparedStatement to avoid sql injection

--- src/com/cdip/java/myshop/dao/OwnerDAO.java
+++ src/com/cdip/java/myshop/dao/OwnerDAO.java
@@ -3,27 +3,31 @@ package com.cdip.java.myshop.dao;
 import com.cdip.java.myshop.db.ConnectionFactory;
 import com.cdip.java.myshop.model.Owner;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class OwnerDAO {
+
+    private static String INSERT_QUERY = "INSERT INTO owner(name, phone_number) VALUES(?, ?)";
 
     public Owner save(Owner owner) {
         // save owner in database
         // connect to database
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+
         try {
             connection = ConnectionFactory.getConnection();
-            statement = connection.createStatement();
-            statement.executeUpdate("INSERT INTO owner(name, phone_number) " +
-                    "VALUES ( '" + owner.getName() + "', " + owner.getPhoneNumber() + ")");
+            preparedStatement = connection.prepareStatement(INSERT_QUERY);
+            preparedStatement.setString(1, owner.getName());
+            preparedStatement.setLong(2, owner.getPhoneNumber());
+
+            preparedStatement.executeUpdate();
+
         } catch (Exception ex) {
             throw new RuntimeException("Error creating owner: ", ex);
         } finally {
             try {
-                statement.close();
+                preparedStatement.close();
                 connection.close();
             } catch (Exception e) { /* ignored */ }
         }
